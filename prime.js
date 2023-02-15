895ca949711092c7b2cd51b7bcd39a9380573cc5
vikasvats <vikas202319@gmail.com>
2023-02-15 19:16:17 +0530

    code check
    hey rutuja please check the code

---
+++ prime.js
@@ -0,0 +1,14 @@
+// plesae check the code it may be wrong
+let num = 14;
+let count = 0;
+
+for(let i = 0; i<=num; i++){
+	if(num%i = 0){
+		c++;
+      }
+}
+if(count == 0){
+	console.log("crime");
+}
+else{
+	console.log("Not crime")
