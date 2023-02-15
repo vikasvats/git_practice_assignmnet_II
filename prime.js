cbb02b568fc508f07e2d6878219c96dbc19fcd88
Rutuja Umap <umaprutuja740@gmail.com>
2023-02-15 19:02:44 +0530

    I added prime program

---
+++ prime.js
@@ -0,0 +1,14 @@
+let num = 14;
+let count = 0;
+
+for(let i = 0; i<=num; i++){
+	if(num%i != 0){
+		count++;
+      }
+}
+if(count == 0){
+	console.log("Prime");
+}
+else{
+	console.log("Not Prime")
+}
